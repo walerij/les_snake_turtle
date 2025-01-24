63c0eb6dc586c942716b12a64468810b6cb61598
ZHdanov Valery <becwal@yandex.ru>
2025-01-24 19:13:36 +0300

    параметры игры и создание экрана

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ import random
 delay = 0.1
 score = 0
 high_score = 0
+
+
+wn = turtle.Screen()
+wn.title("Змейка")
+wn.bgcolor("#3bad54")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+wn.mainloop()
+
