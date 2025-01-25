d61e972e5f05ed287c141370cfeb3401e59c8938
ZHdanov Valery <becwal@yandex.ru>
2025-01-25 08:40:18 +0300

    голова еда и надпись

--- main.py
+++ main.py
@@ -14,5 +14,38 @@ wn.bgcolor("#3bad54")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
+
+head = turtle.Turtle()
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0)
+head.direction = "Stop"
+head.pendown()
+head.dot()
+head.penup()
+
+
+# food in the game
+food = turtle.Turtle()
+colors = random.choice(['red', 'green', 'black'])
+shapes = random.choice(['square', 'triangle', 'circle'])
+food.speed(0)
+food.shape(shapes)
+food.color(colors)
+food.penup()
+food.goto(0, 100)
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+pen.write("Score : 0  High Score : 0", align="center",
+          font=("candara", 24, "bold"))
+
+
 wn.mainloop()
 
